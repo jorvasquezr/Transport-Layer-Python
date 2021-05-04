a11900714cdf8beec6e11ba6e6753c38afd43d6e
Jorge Vasquez <jorvasquezr@gmail.com>
2021-05-04 13:34:24 -0600

    colorame import error

--- apps/ClipConverter/app/configuration.py
+++ apps/ClipConverter/app/configuration.py
@@ -1,18 +1,17 @@
 import os
 try:
     from colorama import init, Fore
+    CLIENT = f'{Fore.GREEN}[CLIENT]{Fore.RESET}: '
+    SERVER = f'{Fore.BLUE}[SERVER]{Fore.RESET}: '
+    ERROR = f'{Fore.RED}[ERROR]{Fore.RESET}: '
 except ImportError:
-    pass
+    CLIENT = '[CLIENT]: '
+    SERVER = '[SERVER]: '
+    ERROR = '[ERROR]: '
+    
 FORMAT = "utf-8"
 BUFFER_SIZE = 1024
 FOLDER_PATH = f"{os.path.dirname(os.path.realpath(__file__)) }/../client_data"
 
-try:
-    CLIENT = f'{Fore.GREEN}[CLIENT]{Fore.RESET}: '
-    SERVER = f'{Fore.BLUE}[SERVER]{Fore.RESET}: '
-    ERROR = f'{Fore.RED}[ERROR]{Fore.RESET}: '
-except:
-    CLIENT = '[CLIENT]: '
-    SERVER = '[SERVER]: '
-    ERROR = '[ERROR]: '
+
     
--- apps/ClipConverter_server/app/configuration.py
+++ apps/ClipConverter_server/app/configuration.py
@@ -2,21 +2,16 @@ import os
 
 try:
     from colorama import init, Fore
+    CONNECTED = f"{Fore.GREEN}[CONNECTED]{Fore.RESET}"
+    DISCONNECTED = f"{Fore.GREEN}[CONNECTED]{Fore.RESET}"
+    SERVERSTATED =f"{Fore.BLUE}[SEVER STARTED]{Fore.RESET}" 
 except ImportError:
-    pass
+    CONNECTED = "[CONNECTED]"
+    DISCONNECTED = "[CONNECTED]"
+    SERVERSTATED ="[SEVER STARTED]" 
 
 TCP_PORT = 5000
 TCP_IP = "0.0.0.0"
 BUFFER_SIZE = 1024
 FORMAT = "utf-8"
 FOLDER_PATH = f"{os.path.dirname(os.path.realpath(__file__)) }/../server_data"
-
-try:
-    CONNECTED = f"{Fore.GREEN}[CONNECTED]{Fore.RESET}"
-    DISCONNECTED = f"{Fore.GREEN}[CONNECTED]{Fore.RESET}"
-    SERVERSTATED =f"{Fore.BLUE}[SEVER STARTED]{Fore.RESET}" 
-except:
-    CONNECTED = "[CONNECTED]"
-    DISCONNECTED = "[CONNECTED]"
-    SERVERSTATED ="[SEVER STARTED]" 
-
